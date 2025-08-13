d344e04b7129bd30f951d646addda198acee1b16
pat123 <phollakrit@hotmail.com>
2025-08-13 14:28:03 +0700

    create refs-array

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import { EventData2 } from './event-data';
+import RefsArray from './refs-array';
 
 
 
 function App() {
-  return <EventData2/>
-   
+  return <RefsArray/>
 }
 
 export default App;
